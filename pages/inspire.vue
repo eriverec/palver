03aeda1cd840406e6f441a2d13df021b41dadab2
Eri Vera <59424621+eriverec@users.noreply.github.com>
2020-09-01 08:00:26 -0500

    new

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,184 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-row :align="alignment" class="grey lighten-5">
+          <v-col md="6" lg="4">
+            <v-card :loading="loading" class="mx-auto my-12">
+              <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+
+              <v-card-title>Cafe Badilico</v-card-title>
+
+              <v-card-text>
+                <v-row align="center" class="mx-0">
+                  <v-rating :value="4.5" color="amber" dense half-increments readonly size="14"></v-rating>
+
+                  <div class="grey--text ml-4">4.5 (413)</div>
+                </v-row>
+
+                <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
+
+                <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+              </v-card-text>
+
+              <v-divider class="mx-4"></v-divider>
+
+              <v-card-title>Tonight's availability</v-card-title>
+
+              <v-card-text>
+                <v-chip-group
+                  v-model="selection"
+                  active-class="deep-purple accent-4 white--text"
+                  column
+                >
+                  <v-chip>5:30PM</v-chip>
+
+                  <v-chip>7:30PM</v-chip>
+
+                  <v-chip>8:00PM</v-chip>
+
+                  <v-chip>9:00PM</v-chip>
+                </v-chip-group>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn color="deep-purple lighten-2" text @click="reserve">Reserve</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col md="6" lg="4">
+            <v-card :loading="loading" class="mx-auto my-12">
+              <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+
+              <v-card-title>Cafe Badilico</v-card-title>
+
+              <v-card-text>
+                <v-row align="center" class="mx-0">
+                  <v-rating :value="4.5" color="amber" dense half-increments readonly size="14"></v-rating>
+
+                  <div class="grey--text ml-4">4.5 (413)</div>
+                </v-row>
+
+                <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
+
+                <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+              </v-card-text>
+
+              <v-divider class="mx-4"></v-divider>
+
+              <v-card-title>Tonight's availability</v-card-title>
+
+              <v-card-text>
+                <v-chip-group
+                  v-model="selection"
+                  active-class="deep-purple accent-4 white--text"
+                  column
+                >
+                  <v-chip>5:30PM</v-chip>
+
+                  <v-chip>7:30PM</v-chip>
+
+                  <v-chip>8:00PM</v-chip>
+
+                  <v-chip>9:00PM</v-chip>
+                </v-chip-group>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn color="deep-purple lighten-2" text @click="reserve">Reserve</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col md="6" lg="4">
+            <v-card :loading="loading" class="mx-auto my-12">
+              <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+
+              <v-card-title>Cafe Badilico</v-card-title>
+
+              <v-card-text>
+                <v-row align="center" class="mx-0">
+                  <v-rating :value="4.5" color="amber" dense half-increments readonly size="14"></v-rating>
+
+                  <div class="grey--text ml-4">4.5 (413)</div>
+                </v-row>
+
+                <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
+
+                <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+              </v-card-text>
+
+              <v-divider class="mx-4"></v-divider>
+
+              <v-card-title>Tonight's availability</v-card-title>
+
+              <v-card-text>
+                <v-chip-group
+                  v-model="selection"
+                  active-class="deep-purple accent-4 white--text"
+                  column
+                >
+                  <v-chip>5:30PM</v-chip>
+
+                  <v-chip>7:30PM</v-chip>
+
+                  <v-chip>8:00PM</v-chip>
+
+                  <v-chip>9:00PM</v-chip>
+                </v-chip-group>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn color="deep-purple lighten-2" text @click="reserve">Reserve</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col md="6" lg="4">
+            <v-card :loading="loading" class="mx-auto my-12">
+              <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+
+              <v-card-title>Cafe Badilico</v-card-title>
+
+              <v-card-text>
+                <v-row align="center" class="mx-0">
+                  <v-rating :value="4.5" color="amber" dense half-increments readonly size="14"></v-rating>
+
+                  <div class="grey--text ml-4">4.5 (413)</div>
+                </v-row>
+
+                <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
+
+                <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+              </v-card-text>
+
+              <v-divider class="mx-4"></v-divider>
+
+              <v-card-title>Tonight's availability</v-card-title>
+
+              <v-card-text>
+                <v-chip-group
+                  v-model="selection"
+                  active-class="deep-purple accent-4 white--text"
+                  column
+                >
+                  <v-chip>5:30PM</v-chip>
+
+                  <v-chip>7:30PM</v-chip>
+
+                  <v-chip>8:00PM</v-chip>
+
+                  <v-chip>9:00PM</v-chip>
+                </v-chip-group>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn color="deep-purple lighten-2" text @click="reserve">Reserve</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+
